9530855e5a474b72a1b8c8b77ba40814f7ea2e04
mahmudft <fatullayevm@gmail.com>
2023-04-24 22:26:36 +0400

    project gen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import QTable from "./components/table/QTable";
+
 
 function App() {
+
+  // const filterTodos = () => {
+  //   return null
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main">
+        <div className="navbar">
+          <div style={{lineHeight: '0px'}}>
+            <h4>Campaigns</h4>
+            <span className="subtext">
+              <i>
+                You can communicate with your customers directly fom this
+                section
+              </i>
+            </span>
+          </div>
+          <Button
+            variant="contained"
+            size="small"
+            style={{ backgroundColor: "#83c26e" }}
+            // onClick={handleClick}
+          >
+            <Link to="/thread">New Thread</Link>
+          </Button>
+        </div>
+        <div className="table-section">
+          <QTable />
+        </div>
+      </div>
   );
 }
 
